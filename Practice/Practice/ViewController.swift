3581b59cd12404deb88ef00e136b84e39e7a2e80
bharatsilavat96 <120012569+bharatsilavat96@users.noreply.github.com>
2022-12-07 15:22:55 +0530

    first commit

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Bharat Silavat on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
