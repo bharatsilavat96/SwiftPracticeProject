16e5f74d4993f49814050beac74bc3e15951ea1e
bharatsilavat96 <120012569+bharatsilavat96@users.noreply.github.com>
2022-12-09 11:39:54 +0530

    8/12/22 - UIViewSessionCommit

--- Practice/Practice/ViewController.swift
+++ Practice/Practice/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var sampleButtonj: UIButton!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func segmentValueChanged(_ sender: Any) {
+    }
+    @IBAction func textfieldDidEndEditing(_ sender: Any) {
+        
+    }
 }
 
